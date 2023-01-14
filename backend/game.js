528fea86ece6466251da7545ec570e415be4a37c
Jonathan Loh <jonathanloh02@gmail.com>
2023-01-14 20:20:10 +0800

    Handle shrink and PVP

--- backend/game.js
+++ backend/game.js
@@ -3,11 +3,17 @@ import { Maze } from "./maze.js";
 
 const LOOP_FPS = 5;
 const MAX_LATENCY_MS = 200;
-const MAX_STATE_CACHE_SIZE = Math.ceil(MAX_LATENCY_MS * (1000 / LOOP_FPS));
+
+const ACTION_COOLDOWN_MS = 1000;
+
+//Derived Values
+const MS_PER_LOOP = 1000 / LOOP_FPS;
+const MAX_STATE_CACHE_SIZE = Math.ceil(MAX_LATENCY_MS / MS_PER_LOOP);
+const COOLDOWN_LOOPS = Math.ceil(ACTION_COOLDOWN_MS / MS_PER_LOOP);
 
 //TODO time based event to shrink maze or end game
 export class GameState {
-  constructor(roomId) {
+  constructor(roomId, mazeSize) {
     this.roomId = roomId;
     this.stateCache = [];
 
@@ -16,11 +22,15 @@ export class GameState {
     this.serverTicks = 0;
     this.loop = new Loop(() => this.updatePositions(), LOOP_FPS);
 
-    this.maze = new Maze(20, 20);
+    this.maze = new Maze(mazeSize, mazeSize);
+    this.mazeBounds = [
+      [0, mazeSize - 1],
+      [0, mazeSize - 1],
+    ]; //Inclusive
+    this.maze.generate();
   }
 
   startGame() {
-    this.maze.generate();
     global.rooms[this.roomId].users.forEach((socketId) => {
       this.packetNumbers[socketId] = -1;
       this.locations[socketId] = [0, 0];
@@ -47,6 +57,92 @@ export class GameState {
   }
 
   processInput(socketId, serverTicks, newInput, packetNum) {
+    const [x, y] = this.locations[socketId];
+    if ((1 << 0) & newInput) {
+      if (!this.maze.horiz[y][x] && x > this.mazeBounds[0][0])
+        this.locations[socketId][0]--;
+    }
+    if ((1 << 1) & newInput) {
+      if (!this.maze.horiz[y][x + 1] && x < this.mazeBounds[0][1])
+        this.locations[socketId][0]++;
+    }
+    if ((1 << 2) & newInput) {
+      if (!this.maze.vert[y][x] && y > this.mazeBounds[1][0])
+        this.locations[socketId][1]--;
+    }
+    if ((1 << 3) & newInput) {
+      if (!this.maze.vert[y + 1][x] && y < this.mazeBounds[1][1])
+        this.locations[socketId][1]++;
+    }
+
+    this.packetNumbers[socketId] = packetNum;
+  }
+
+  removePlayer(socketId) {
+    delete this.packetNumbers[socketId];
+    delete this.locations[socketId];
+  }
+}
+
+const TIME_SHRINK_MS = 30000; //in ms
+
+class PVPGameState extends GameState {
+  constructor(roomId, mazeSize) {
+    super(roomId, mazeSize);
+    this.lives = {};
+    this.cooldown = {};
+
+    this.NEXT_TIME_SHRINK_MS = 0;
+    this.incrementTimeShrink();
+  }
+
+  async incrementTimeShrink() {
+    this.NEXT_TIME_SHRINK_MS += TIME_SHRINK_MS;
+    this.NEXT_TIME_SHRINK_LOOP = Math.ceil(
+      this.NEXT_TIME_SHRINK_MS / MS_PER_LOOP
+    );
+  }
+
+  startGame() {
+    global.rooms[this.roomId].users.forEach((socketId) => {
+      this.lives[socketId] = 3;
+      this.cooldown[socketId] = -1;
+    });
+    super.startGame();
+  }
+
+  updatePositions() {
+    super.updatePositions();
+    const playersLeft = Object.values(this.lives).filter((v) => v >= 0).length;
+
+    if (this.serverTicks >= this.NEXT_TIME_SHRINK_LOOP) {
+      global.io.to(this.roomId).emit("shrinkMaze", this.mazeBounds[0][1]);
+
+      this.mazeBounds = this.mazeBounds.reduce((curr, v) => {
+        curr.push([v[0] + 1, v[1] - 1]);
+        return curr;
+      }, []);
+
+      this.locations.forEach((socketId, [x, y]) => {
+        if (x < this.mazeBounds[0][0] || x > this.mazeBounds[0][1])
+          this.lives[socketId] = 0;
+        if (y < this.mazeBounds[1][0] || y > this.mazeBounds[1][1])
+          this.lives[socketId] = 0;
+      });
+      this.incrementTimeShrink();
+    }
+
+    for (const [socketId, lives] of Object.entries(this.lives)) {
+      global.io.to(socketId).emit("updateLives", {
+        lives,
+        playersLeft,
+      });
+    }
+  }
+
+  processInput(socketId, serverTicks, newInput, packetNum) {
+    super.processInput(socketId, serverTicks, newInput, packetNum);
+
     //Void input if latency from client too high
     if (this.stateCache[0] && serverTicks < this.stateCache[0].serverTicks) {
       console.warn(
@@ -59,52 +155,66 @@ export class GameState {
       return;
     }
 
-    const [x, y] = this.locations[socketId];
-    if ((1 << 0) & newInput) {
-      if (!this.maze.horiz[y][x]) this.locations[socketId][0]--;
-    }
-    if ((1 << 1) & newInput) {
-      if (!this.maze.horiz[y][x + 1]) this.locations[socketId][0]++;
-    }
-    if ((1 << 2) & newInput) {
-      if (!this.maze.vert[y][x]) this.locations[socketId][1]--;
-    }
-    if ((1 << 3) & newInput) {
-      if (!this.maze.vert[y + 1][x]) this.locations[socketId][1]++;
-    }
+    //Space bar action
+    if ((1 << 4) & newInput) {
+      //Cooldown handling
+      if (serverTicks < this.cooldown[socketId]) return;
 
-    this.packetNumbers[socketId] = packetNum;
+      const locations = this.stateCache.filter(
+        (v) => v.serverTicks === serverTicks
+      )[0];
+
+      const [x, y] = locations[socketId];
+      Object.entries(locations).forEach((oppSocketId, [oppX, oppY]) => {
+        if (oppSocketId === socketId) return;
+        if (Math.abs(x - oppX) > 1) return;
+        if (Math.abs(y - oppY) > 1) return;
+
+        this.lives[oppSocketId]--;
+        this.cooldown[socketId] = this.serverTicks + COOLDOWN_LOOPS;
+      });
+    }
   }
 
   removePlayer(socketId) {
-    delete this.packetNumbers[socketId];
-    delete this.locations[socketId];
+    delete this.lives[socketId];
   }
 }
 
-export function createGame(roomId) {
-  global.gameStates[roomId] = new GameState(roomId);
+class EscapeGameState extends GameState {
+  constructor(roomId, mazeSize) {
+    super(roomId, mazeSize);
+  }
 }
 
 export function startGameListeners(socket) {
   socket.on("startGame", () => {
-    Array.from(socket.rooms).forEach((roomId) => {
-      if (roomId === socket.id) return;
+    const arr = Array.from(socket.rooms);
+    for (const roomId of arr) {
+      if (roomId === socket.id) continue;
+
+      if (global.rooms[roomId].gameMode === "PVP")
+        global.gameStates[roomId] = new PVPGameState(roomId, 20);
+      else global.gameStates[roomId] = new EscapeGameState(roomId, 20);
+
       global.gameStates[roomId].startGame();
       const maze = global.gameStates[roomId].maze;
       global.io.to(roomId).emit("maze", [maze.horiz, maze.vert]);
-    });
+      return;
+    }
   });
 
   socket.on("playerInput", ({ packetNumber, serverTicks, input }) => {
-    Array.from(socket.rooms).forEach((roomId) => {
-      if (roomId === socket.id) return;
+    const arr = Array.from(socket.rooms);
+    for (const roomId of arr) {
+      if (roomId === socket.id) continue;
       global.gameStates[roomId].processInput(
         socket.id,
         serverTicks,
         input,
         packetNumber
       );
-    });
+      return;
+    }
   });
 }
