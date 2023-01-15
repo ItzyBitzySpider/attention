4e3f98bcbc5564382b4c0038d50990dce1522bf5
Jonathan Loh <jonathanloh02@gmail.com>
2023-01-15 09:06:03 +0800

    Workaround spectator player locations

--- backend/gamestate.js
+++ backend/gamestate.js
@@ -64,6 +64,7 @@ export class GameState {
       // console.log(socketId, packetNum, tmpState);
       global.io.to(socketId).emit("playerLocations", tmpState);
     }
+    currentState.locations = JSON.stringify(currentState.locations);
     global.io.to(this.roomId).emit("playerLocationsSpectator", currentState);
     this.stateCache.push(currentState);
     if (this.stateCache.length > MAX_STATE_CACHE_SIZE) this.stateCache.shift();
